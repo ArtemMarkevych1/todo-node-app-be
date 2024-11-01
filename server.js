d792cf356792a9d37e8e99e3ea28c35c0d825f70
amarkevych <artem@govplus.com>
2024-11-01 16:11:14 +0200

    Add CORS settings

--- server.js
+++ server.js
@@ -11,11 +11,32 @@ mongoose.connect(process.env.MONGODB_CONNECT_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-app.use(cors());
+// CORS configuration
+const corsOptions = {
+  origin: ['https://todo-node-app.netlify.app', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/todos', todoRouter);
+
+// Add this error handler after your routes
+app.use((err, req, res, next) => {
+  if (err.name === 'CORSError') {
+    res.status(403).json({
+      error: 'CORS error',
+      message: 'Not allowed by CORS'
+    });
+  } else {
+    next(err);
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
